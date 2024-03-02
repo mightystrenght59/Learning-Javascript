933b880da7c7380f3dcb90e4b7db53e14252c94f
Jesse Jordan <130009445+mightystrenght59@users.noreply.github.com>
2024-03-02 20:13:25 +0000

    My first Javascript Project

---
+++ app.js
@@ -0,0 +1,10 @@
+ let userInput = prompt("What is your password");
+let countPass = userInput.length;
+
+if (countPass < 4) {
+    console.log("Password is too short");
+} else if (countPass > 4) {
+    console.log("Password is too long");
+} else {
+    console.log("Lovely");
+}
