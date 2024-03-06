3fd64f40b5bfc25c538deea503fee7b6e82557dd
Jesse Jordan <130009445+mightystrenght59@users.noreply.github.com>
2024-03-06 09:36:53 -0400

    Update script.js
    Array , Push and Unshift Method

---
+++ script.js
@@ -0,0 +1,7 @@
+let person = ["Jesse","Nathan","Johanna","Jaheem"];
+let age = [22,13,24,16];
+// Unshift is use to add an element at the begining of an Array.
+age.unshift(12);
+// Push is use to add and element to the end of an array.
+age.push(24);
+console.log(age);
